1e76e2dc23235c4a33579739e43c9f3ea56bb798
liudonghua123 <liudonghua123@gmail.com>
2017-01-21 19:53:31 +0800

    work completed

--- src/shop.js
+++ src/shop.js
@@ -26,42 +26,93 @@ const expressJwtOptions = {
 
 // 获取指定商户信息
 // GET /shop/:shopId?token=TOKEN
-router.get('/:shopId', expressJwt(expressJwtOptions),async (req, res) => {
-  const shopId = req.params.shopId;
-  // const shop = await yktManager.getShop(shopId);
-  const shop = {shopid: 123, shopname: 'shop123'};
-  res.json({ ret: 0, data: shop });
+router.get('/:shopId', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const shopId = req.params.shopId;
+    const shop = await yktManager.getShop(shopId);
+    res.json({ ret: 0, data: shop });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
 });
 
 // 获取指定商户日账单
 // GET /shop/:shopId/daily-bill/:accDate?token=TOKEN
-router.get('/:shopId/daily-bill/:accDate', expressJwt(expressJwtOptions),async (req, res) => {
-  const shopId = req.params.shopId;
-  const accDate = req.params.accDate;
-  const shopBill = await yktManager.getShopBill(shopId, accDate);
-  res.json({ ret: 0, data: shopBill });
+router.get('/:shopId/daily-bill/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const shopId = req.params.shopId;
+    const accDate = req.params.accDate;
+    const shopBill = await yktManager.getShopBill(shopId, accDate);
+    res.json({ ret: 0, data: shopBill });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
 });
 
 // 获取子商户日账单列表
 // GET /shop/:fShopId/sub-shop-daily-bills/:accDate?token=TOKEN
-router.get('/:fShopId/sub-shop-daily-bills/:accDate', expressJwt(expressJwtOptions),async (req, res) => {
-  const fShopId = req.params.fShopId;
-  const accDate = req.params.accDate;
-  const shopBills = await yktManager.getShopBills(fShopId, accDate);
-  res.json({ ret: 0, data: shopBills });
+router.get('/:fShopId/sub-shop-daily-bills/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const fShopId = req.params.fShopId;
+    const accDate = req.params.accDate;
+    const shopBills = await yktManager.getShopBills(fShopId, accDate);
+    res.json({ ret: 0, data: shopBills });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
 });
 
 // 获取所属商户的设备的日账单列表
 // GET /shop/:shopId/device-daily-bills/:accDate?token=TOKEN
-router.get('/:shopId/device-daily-bills/:accDate', expressJwt(expressJwtOptions),async (req, res) => {
-  const shopId = req.params.shopId;
-  const accDate = req.params.accDate;
-  const shopBills = await yktManager.getDeviceBills(shopId, accDate);
-  res.json({ ret: 0, data: shopBills });
+router.get('/:shopId/device-daily-bills/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const shopId = req.params.shopId;
+    const accDate = req.params.accDate;
+    const shopBills = await yktManager.getDeviceBills(shopId, accDate);
+    res.json({ ret: 0, data: shopBills });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
 });
 
 // 获取商户月账单
 // GET /shop/:shopId/monthly-bill/:accDate?token=TOKEN
+router.get('/:shopId/monthly-bill/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const shopId = req.params.shopId;
+    const accDate = req.params.accDate;
+    const shopBill = await yktManager.getShopBillMonth(shopId, accDate);
+    res.json({ ret: 0, data: shopBill });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
+});
 
+// 获取子商户月账单列表
+// GET /shop/:fShopId/sub-shop-monthly-bills/:accDate?token=TOKEN
+router.get('/:fShopId/sub-shop-monthly-bills/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const fShopId = req.params.fShopId;
+    const accDate = req.params.accDate;
+    const shopBills = await yktManager.getShopBillsMonth(fShopId, accDate);
+    res.json({ ret: 0, data: shopBills });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
+});
+
+// 获取所属商户的设备的月账单列表
+// GET /shop/:shopId/device-monthly-bills/:accDate?token=TOKEN
+router.get('/:shopId/device-monthly-bill/:accDate', expressJwt(expressJwtOptions), async (req, res) => {
+  try {
+    const shopId = req.params.shopId;
+    const accDate = req.params.accDate;
+    // const deviceBills = await yktManager.getDeviceBillsMonth(shopId, accDate);
+    const deviceBills = {};
+    res.json({ ret: 0, data: deviceBills });
+  } catch (err) {
+    res.json({ ret: 500, data: err });
+  }
+});
 
 export default router;
