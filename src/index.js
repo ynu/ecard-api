f4775a13c331e75763b630f4713eab095ecc0455
liudonghua123 <liudonghua123@gmail.com>
2019-02-26 11:22:09 +0800

    fix static serve path

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import path from 'path';
 import { host, port } from './config';
 import shop from './shop';
 import operator from './operator';
@@ -29,7 +30,7 @@ app.use(cookieParser('my cookie key'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.resolve(path.join(__dirname, '../public'))));
 
 const morgan = require('morgan');
 app.use(morgan('dev'));
@@ -42,20 +43,16 @@ app.use('/operator', operator);
 app.use('/custcardinfo', custcardinfo);
 app.use('/userdtl', userdtl);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
-});
-
 /*
  处理异常
  */
 app.use(async (err, req, res, next) => {
   console.log(err);
-  res.json({ret: err.status, data: err.message});
+  res.json({ ret: err.status, data: err.message });
 });
 
 app.listen(port, () => {
   console.log(`The server is running at http://${host}/`);
 });
 
-//export YktManager from './YktManager.js';
+// export YktManager from './YktManager.js';
